7db17692e57493392467ebdc563d3cfa367064f9
RSADenchik <WorldRSA@gmail.com>
2016-09-30 18:51:56 +0300

    First_commit

---
+++ js/main.js
@@ -0,0 +1,39 @@
+$(document).ready( function () {
+   /* Slider-slick */
+    $('.slider-slick').slick({
+        dots: false,
+        variableWidth: false,
+        infinite: true,
+        speed: 700,
+        prevArrow: "<img class='arrow arrow-fix slick-prev arrow-left' src='img/arrow-left.png'>",
+        nextArrow: "<img class='arrow arrow-fix slick-next arrow-right' src='img/arrow-right.png'>",
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+
+            {
+                breakpoint: 1250,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 873,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+
+        ]
+    });
+    }
+);
